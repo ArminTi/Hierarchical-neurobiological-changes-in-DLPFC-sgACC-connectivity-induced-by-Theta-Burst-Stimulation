3d9a9b3264568f30deec8fecfc78d2eea52c628e
Gh. Ghaffaripour <138153801+Gh-Ghaffaripour@users.noreply.github.com>
2025-01-14 15:30:31 +0400

    Update and rename save_dcm.m to save_dcm_original.m

--- Data Analysis/Functions/save_dcm_original.m
+++ Data Analysis/Functions/save_dcm_original.m
@@ -1,4 +1,4 @@
-function [] = save_dcm(output_folder, d_name, DCM)
+function [] = save_dcm_original(output_folder, d_name, DCM)
     full_path = fullfile(output_folder, [d_name '.mat']);  % Ensure .mat extension
     
     save(full_path, 'DCM');
