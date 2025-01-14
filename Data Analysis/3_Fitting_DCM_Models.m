67d704ef2ba61972f0a43d353d04eff119d0b9ef
Gh. Ghaffaripour <138153801+Gh-Ghaffaripour@users.noreply.github.com>
2025-01-14 15:29:16 +0400

    Update 3_Fitting_DCM_Models.m

--- Data Analysis/3_Fitting_DCM_Models.m
+++ Data Analysis/3_Fitting_DCM_Models.m
@@ -5,27 +5,36 @@ clc;
 restoredefaultpath;
 addpath('C:/Users/Growth fire/Programs/Matlab plugins/spm12/spm12');
 spm('defaults', 'eeg')
-% ------------------------------- path ----------------------------------
-file_path = 'E:/signal_data/DCM_Specified';
-file_name = 'DCM_SPM_sub-*_*_*-rest_run-*_final.mat'; 
+% --------------------------- Define Paths ---------------------------
+file_path = 'D:/signal_data/DCM_Specified/doing';
+file_name = 'DCM_SPM_sub-*_ses-*_epoch-*.mat';  
+% Or choose a specific file to be processed
+%file_name = 'DCM_SPM_sub-09_ses-02_epoch-03_converted_data_itbs.mat';
 files = dir(fullfile(file_path, file_name));
 cd(file_path)
-addpath('E:/signal_data/all_spm_obj');
-addpath('E:/signal_data/code_looped'); % where save_dcm function is saved
 
-output_folder = 'E:/signal_data/DCM_fitted';
+addpath('D:/signal_data/all_spm_obj/doing');
+addpath('D:/signal_data/code_looped'); % function of save_dcm_original
+
+output_folder = 'D:/signal_data/DCM_fitted/doing';
 if ~exist(output_folder, 'dir')
     mkdir(output_folder);
 end
-
-%% -------------- paralell loop for fitting the DCM to data -----------------
 tic
+
+%% ----------------------------- Fitting the DCM to data  --------------------------------
+% paralell loop for fitting 
 parfor idcm = 1:length(files)
-        tmp = load(files(idcm).name, 'DCM');  
-        DCM = tmp.DCM;
-        DCM = spm_dcm_csd(DCM);
-        save_dcm(output_folder, ['fitted_' files(idcm).name(1:end-4)], DCM);
+    files(idcm)
+    tmp = load(files(idcm).name, 'DCM');
+    disp('data loaded')
+    DCM = tmp.DCM;
+    disp('DCM extracted')
+    DCM = spm_dcm_csd(DCM);
+    save_dcm_original(output_folder, ['fitted_' files(idcm).name(1:end-4)], DCM);
 end
 toc
+
 disp('fitting for all subjects are done')
+
 
