41f228f171ff7b1735a343c1b1746b375a1d227f
Gh. Ghaffaripour <138153801+Gh-Ghaffaripour@users.noreply.github.com>
2025-10-02 12:11:37 +0400

    folder

--- dcm_analysis/Functions/explained_var_dcm.m
+++
@@ -1,7 +0,0 @@
-function [x] = explained_var_dcm(DCM)
-
-pss=sum(sum(sum(abs(spm_vec(DCM.Hc)).^2)));
-rss=sum(sum(sum(abs(spm_vec(DCM.Rc)).^2)));
-x= 100*pss/(rss+pss);
-
-end
--- dcm_analysis/Functions/save_dcm_original.m
+++
@@ -1,5 +0,0 @@
-function [] = save_dcm_original(output_folder, d_name, DCM)
-    full_path = fullfile(output_folder, [d_name '.mat']);  % Ensure .mat extension
-    
-    save(full_path, 'DCM');
-end
