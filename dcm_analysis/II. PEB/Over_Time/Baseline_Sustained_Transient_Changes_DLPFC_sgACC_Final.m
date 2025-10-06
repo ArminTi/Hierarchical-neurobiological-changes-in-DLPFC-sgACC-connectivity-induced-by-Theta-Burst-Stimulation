754d06bba13138f78977fdd8f90a9715b0ff9d5c
Gh. Ghaffaripour <138153801+Gh-Ghaffaripour@users.noreply.github.com>
2025-10-06 11:56:57 +0000

    Update and rename DLPFC_sgACC_Final.m to Baseline_Sustained_Transient_Changes_DLPFC_sgACC_Final.m
    cleaned

--- dcm_analysis/II. PEB/Over_Time/Baseline_Sustained_Transient_Changes_DLPFC_sgACC_Final.m
+++ dcm_analysis/II. PEB/Over_Time/Baseline_Sustained_Transient_Changes_DLPFC_sgACC_Final.m
@@ -1,7 +1,7 @@
 %% Loading GCMs
 clear all;
 clc;
-addpath('C:/Users/ASUS/Desktop/Apps/spm12');
+addpath('C:/Users/Documents/Programs/spm_25.01.02/spm');
 spm('defaults', 'eeg'); 
 load('GCM_ctbs.mat');
 load('GCM_itbs.mat');
@@ -41,8 +41,20 @@ niTBS = length(Peb_itbs);
 save('Peb_sham_30min', 'Peb_sham')
 save('Peb_ctbs_30min', "Peb_ctbs")
 save('Peb_itbs_30min', "Peb_itbs")
+Peb_cTBSvsiTBS = [Peb_ctbs; Peb_itbs];
 Peb_cTBSvsSham = [Peb_sham; Peb_ctbs];
 Peb_iTBSvsSham = [Peb_sham; Peb_itbs];
+
+%% peb second level: itbs vs ctbs
+clear M
+
+M = struct();
+M.X = [ones(ncTBS+niTBS,1),[repmat(-1,[ncTBS,1]); repmat(+1,[niTBS,1])]];
+
+cTBSvsiTBS_final = spm_dcm_peb(Peb_cTBSvsiTBS, M, fields);
+BMA_cTBSvsiTBS = spm_dcm_peb_bmc(cTBSvsiTBS_final)
+save( "BMA_cTBSvsiTBS_BST_-1c_1i",'BMA_cTBSvsiTBS')
+%spm_dcm_peb_review(BMA_cTBSvsiTBS);
 
 %% peb second level: itbs
 clear M
@@ -52,31 +64,31 @@ M.X = [ones(ncTBS,1)];
 
 iTBS_final = spm_dcm_peb(Peb_itbs, M, fields);
 BMA_itbs_final = spm_dcm_peb_bmc(iTBS_final);
-save('BMA_itbs_overtime', 'BMA_itbs_final')
-spm_dcm_peb_review(BMA_itbs_final);
-
+save( 'BMA_itbs_final_BST_n1i','BMA_itbs_final')
+%spm_dcm_peb_review(BMA_itbs_final);
 %% peb second level: ctbs
 
 cTBS_final = spm_dcm_peb(Peb_ctbs, M, fields);
 BMA_ctbs_final = spm_dcm_peb_bmc(cTBS_final);
-save('BMA_ctbs_overtime', 'BMA_ctbs_final')
-spm_dcm_peb_review(BMA_ctbs_final);
+save( 'BMA_ctbs_final_BST_n1c', 'BMA_ctbs_final')
+%spm_dcm_peb_review(BMA_ctbs_final);
 
 %% peb second level: sham
 
 sham_final = spm_dcm_peb(Peb_sham, M, fields);
 BMA_sham_final = spm_dcm_peb_bmc(sham_final);
-save('BMA_sham_overtime', 'BMA_sham_final')
-spm_dcm_peb_review(BMA_sham_final);
+save( 'BMA_sham_final_BST_n1s', 'BMA_sham_final')
+%spm_dcm_peb_review(BMA_sham_final);
 
 %% peb second level: itbs vs sham
 clear M
 
 M = struct();
-M.X = [ones(ncTBS+nSham,1),[repmat(-1,[ncTBS,1]); repmat(+1,[ncTBS,1])]];
+M.X = [ones(niTBS+nSham,1),[repmat(-1,[niTBS,1]); repmat(+1,[niTBS,1])]];
 
 iTBSvsSham_final = spm_dcm_peb(Peb_iTBSvsSham, M, fields);
 BMA_itbs = spm_dcm_peb_bmc(iTBSvsSham_final);
+save( 'BMA_itbs_BST_-1s_1i','BMA_itbs')
 
 
 %% peb second level: ctbs vs sham
@@ -87,6 +99,7 @@ M.X = [ones(ncTBS+nSham,1),[repmat(-1,[ncTBS,1]); repmat(+1,[ncTBS,1])]];
 
 cTBSvsSham_final = spm_dcm_peb(Peb_cTBSvsSham, M, fields);
 BMA_ctbs = spm_dcm_peb_bmc(cTBSvsSham_final);
+save('BMA_ctbs_BST_-1s_1c','BMA_ctbs')
 
 %% Review
 
